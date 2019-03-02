96169363cdf013fb377ec34b153de6a3e93384a0
caolong <cl@jackyun.com>
2019-03-02 15:40:07 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">hello word</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped  lang="stylus" rel="stylesheet/stylus" >
+@import 'common/stylus/variable';
+
+#app {
+  color: $color-theme;
+}
+</style>
