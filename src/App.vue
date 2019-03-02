405f7753b4677f77c8fd2abf6783a6af19f10819
jjcl521 <jjcl521@163.com>
2019-03-02 17:41:47 +0800

    1，header功能 logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="app">hello word</div>
+  <div id="app">
+    <m-header></m-header>
+  </div>
 </template>
 
-<script>
-
+<script type='text/ecmascript-6'>
+import MHeader from 'components/m-header/m-header.vue'
+export default {
+  components: {
+    MHeader
+  }
+}
 </script>
 
 <style scoped  lang="stylus" rel="stylesheet/stylus" >
