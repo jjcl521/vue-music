df4505ff7852c2549d43618a4363dca882dfba44
jjcl521 <jjcl521@163.com>
2019-03-02 18:34:42 +0800

    router 导航添加完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import Recommend from 'components/recommend/recommend'
+import Rank from 'components/rank/rank'
+import Singer from 'components/singer/singer'
+import Search from 'components/search/search'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      name: 'HelloWorld',
+      component: Recommend
+    },
+    {
+      path: '/rank',
+      name: 'Rank',
+      component: Rank
+    },
+    {
+      path: '/singer',
+      name: 'Singer',
+      component: Singer
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    }
   ]
 })
