b59d1e1aae1c2bde5969d825a1ae2ff124fa6ee1
曹龙 <cl@differ.com>
2019-03-04 16:06:09 +0800

    eslint规则重新设置

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,24 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    //强制使用单引号
-    quotes: ["error", "single"],
-    //强制不使用分号结尾
-    semi: ["error", "never"]
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-unused-vars": [2, {
+      // 允许声明未使用变量
+      "vars": "local",
+      // 参数不检查
+      //"args": "none"
+    }],
+    // 关闭语句强制分号结尾
+    "semi": [0],
+    //空行最多不能超过100行
+    "no-multiple-empty-lines": [0, { "max": 100 }],
+    //关闭禁止混用tab和空格
+    "no-mixed-spaces-and-tabs": [0],
   },
   parserOptions: {
     parser: "babel-eslint"
