d202c1693a22a83cc67e04d82d414e4f0b159623
曹龙 <cl@differ.com>
2019-03-05 20:09:50 +0800

    1,devServer proxyTable

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/qq': {
+        target: 'http://c.y.qq.com',
+        //changeOrigin: true,
+        pathRewrite: {
+          '^/qq': ''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
